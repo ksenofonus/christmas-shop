811b366b9b07f059cc2e60fedfb8d19d52dccbc6
ksenofonus <danilova.ka88@yandex.ru>
2024-11-24 21:29:46 +0400

    refactor: timer

--- christmas-shop/assets/js/timer.js
+++ christmas-shop/assets/js/timer.js
@@ -6,11 +6,10 @@ const timer = () => {
 	const hours = document.querySelector('.timer_hours');
 	const minutes = document.querySelector('.timer_minutes');
 	const seconds = document.querySelector('.timer_seconds');
-
 	const dayNumber = dif / (1000 * 24 * 60 * 60);
-	const hoursNumber = (dayNumber - Math.floor(dayNumber)) * 24;
-	const minutesNumber = (hoursNumber - Math.floor(hoursNumber)) * 60;
-	const secondsNumber = (minutesNumber - Math.floor(minutesNumber)) * 60;
+	const hoursNumber = Math.floor(dif / (1000 * 60 * 60)) % 24;
+	const minutesNumber = Math.floor(dif / (1000 * 60)) % 60 ;
+	const secondsNumber = Math.floor(dif / 1000) % 60;
 	if (Math.round(secondsNumber) === 60) {
 		seconds.textContent = '0';
 		minutes.textContent = Math.floor(minutesNumber) + 1;
@@ -22,6 +21,4 @@ const timer = () => {
 	hours.textContent = Math.floor(hoursNumber);
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  setInterval(() => timer(), 1000);
-} )
+setInterval(() => timer(), 1000);
