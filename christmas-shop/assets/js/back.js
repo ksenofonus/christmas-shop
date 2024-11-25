5928a87432c38149da988b971a0ea47786187e05
ksenofonus <danilova.ka88@yandex.ru>
2024-11-25 19:25:34 +0400

    feat: add no-scrolling body when modal is open

--- christmas-shop/assets/js/back.js
+++ christmas-shop/assets/js/back.js
@@ -2,10 +2,12 @@ const back = document.createElement('div');
 back.className = 'back';
 const createBack = () => {
   document.body.append(back);
+  document.body.classList.add('no-scroll');
 }
 
 const removeBack = () => {
   back.remove();
+  document.body.classList.remove('no-scroll');
 }
 
 export { createBack, removeBack }
