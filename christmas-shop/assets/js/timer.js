abd001ef8191570cccb7837699a24e0066e00112
ksenofonus <danilova.ka88@yandex.ru>
2024-11-25 10:05:26 +0400

    refactor: timer

--- christmas-shop/assets/js/timer.js
+++ christmas-shop/assets/js/timer.js
@@ -1,19 +1,25 @@
+const targetDate = Date.parse(new Date(2025, 0, 1, 0, 0, 0, 0).toUTCString());
+let timerid = null;
+const day = document.querySelector('.timer_days');
+const hours = document.querySelector('.timer_hours');
+const minutes = document.querySelector('.timer_minutes');
+const seconds = document.querySelector('.timer_seconds');
+
 const timer = () => {
-	const targetDate = Date.parse(new Date(2025, 0, 1, 0, 0, 0, 0).toUTCString());
 	const nowDate = Date.parse(new Date().toUTCString());
 	const dif = targetDate - nowDate;
-	const day = document.querySelector('.timer_days');
-	const hours = document.querySelector('.timer_hours');
-	const minutes = document.querySelector('.timer_minutes');
-	const seconds = document.querySelector('.timer_seconds');
+	if (dif === 0)
+		targetDate = Date.parse(new Date(2026, 0, 1, 0, 0, 0, 0).toUTCString());
+
 	const dayNumber = Math.floor(dif / (1000 * 24 * 60 * 60));
 	const hoursNumber = Math.floor(dif / (1000 * 60 * 60)) % 24;
-	const minutesNumber = Math.floor(dif / (1000 * 60)) % 60 ;
+	const minutesNumber = Math.floor(dif / (1000 * 60)) % 60;
 	const secondsNumber = Math.floor(dif / 1000) % 60;
 	seconds.textContent = secondsNumber;
 	minutes.textContent = minutesNumber;
 	day.textContent = dayNumber;
 	hours.textContent = hoursNumber;
 };
+timer();
 
-setInterval(() => timer(), 1000);
+timerid = setInterval(() => timer(), 1000);
